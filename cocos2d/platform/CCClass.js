3dcf2f01b9113bc4d62e7cafe0a0d2b7a772c6a0
Kang-Hao (Kenny) Lu <kanghaol@oupeng.com>
2013-07-11 11:44:27 +0800

    typo in comments

--- cocos2d/platform/CCClass.js
+++ cocos2d/platform/CCClass.js
@@ -160,8 +160,8 @@ ClassManager.getNewID=function(){
         //         this[p] = this[p];
         //     }
         //
-        // because using for keyed assignment (this[x] = y instead of
-        // this.x = y) is almost certainly going to make an object switch
+        // because using keyed assignment (this[x] = y instead of this.x = y) 
+        // is almost certainly going to make an object switch
         // to dictionary mode in V8. 
         //
         // See https://github.com/oupengsoftware/v8/wiki/Dictionary-mode-%28English%29#wiki-append-property
